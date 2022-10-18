73722d205dfc3321ea26c3f6c68829dd99ccb4fe
Hafiz Muhammad Rayyan <hafizmuhammmadrayyan@gmail.com>
2022-10-18 23:33:48 +0500

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ Array.from(buttons).forEach((button) => {
             string = eval(string);
             document.querySelector('input').value = string;
         }
+        else if (e.target.innerHTML == 'Mc' || e.target.innerHTML == 'Mr') {
+            string = "";
+            document.querySelector('input').value = string;
+        }
         else if (e.target.innerHTML == 'C') {
             string = "";
             document.querySelector('input').value = string;
